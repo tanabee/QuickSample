03d756b0aa5f1ed3add732c09005de9bc5095ea5
yutanabe <yutanabe@yahoo-corp.jp>
2016-04-06 16:10:29 +0900

    first commit

---
+++ QuickSample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  QuickSample
+//
+//  Created by 田邉　裕貴 on 2016/04/06.
+//  Copyright © 2016年 田邉　裕貴. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
